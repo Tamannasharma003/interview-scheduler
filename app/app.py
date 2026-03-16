410699a31ef082294b8f37a087d52168d00b67b2
tamanna <tamanna3333333sharma@gmail.com>
2026-03-16 13:46:50 +0530

    fix webhook verification

--- app/app.py
+++ app/app.py
@@ -1,13 +1,22 @@
-import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Flask webhook running"
+VERIFY_TOKEN = "tamanna_verify_token"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+
+    if request.method == "GET":
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if token == VERIFY_TOKEN:
+            return challenge
+        else:
+            return "Verification failed", 403
+
+    if request.method == "POST":
+        data = request.json
+        print(data)
 
