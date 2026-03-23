458b387c9868872bec03ff669c622e86d525f27b
tamanna <tamanna3333333sharma@gmail.com>
2026-03-23 13:38:49 +0530

    fixed database and webhook final version

--- app/model.py
+++ app/model.py
@@ -5,7 +5,6 @@ class Interview(Base):
     __tablename__ = "interviews"
 
     id = Column(Integer, primary_key=True, index=True)
-    manager_name = Column(String)
     candidate_name = Column(String)
     time_slot = Column(String)
     selected_slot = Column(String)
