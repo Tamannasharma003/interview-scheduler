a99bea7094610a9c5c3681e2b42d11f7159dceee
tamanna <tamanna3333333sharma@gmail.com>
2026-03-25 12:53:33 +0530

    fix app.py duplicate import

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from webhook import app
-from webhook import app
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway needs PORT
+    port = int(os.environ.get("PORT", 8080))  # Railway PORT
     print("🚀 Starting main app...")
     app.run(host="0.0.0.0", port=port)
+
 
