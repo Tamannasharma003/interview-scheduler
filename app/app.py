d71a225ea5af399d4daa4c85d17a2b3761bc215f
tamanna <tamanna3333333sharma@gmail.com>
2026-03-25 16:46:51 +0530

    fixed railway host

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ import webhook
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # Railway PORT
+    print("🚀 Starting main app...")
     app.run(host="0.0.0.0", port=port)
 
