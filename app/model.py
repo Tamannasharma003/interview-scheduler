6032fe56b0fd1ceb88653454842ca6180aa05693
tamanna <tamanna3333333sharma@gmail.com>
2026-03-25 13:19:25 +0530

    fix phone number datatype

--- app/model.py
+++ app/model.py
@@ -5,8 +5,8 @@ class Interview(Base):
     __tablename__ = "interviews"
 
     id = Column(Integer, primary_key=True, index=True)
-    manager_id = Column(Integer, nullable=True)
-    candidate_id = Column(Integer, nullable=True)
+    manager_id = Column(String(20), nullable=True)
+    candidate_id = Column(String(20), nullable=True)
     slots = Column(Text, nullable=True)
     selected_slot = Column(String(255), nullable=True)
     status = Column(String(20), default="pending")
