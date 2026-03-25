df1d6fedde5477cf64dff9b351a17704e6c6f810
tamanna <tamanna3333333sharma@gmail.com>
2026-03-25 16:44:50 +0530

    fixed railway host

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 # Import webhook routes AFTER creating app
 import webhook
 
+
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
