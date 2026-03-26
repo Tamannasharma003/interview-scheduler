9478a66ebf81adaabe48a178a5431f19d36a5970
tamanna <tamanna3333333sharma@gmail.com>
2026-03-26 12:56:44 +0530

    debug credentials

--- app/google_auth.py
+++ app/google_auth.py
@@ -7,6 +7,9 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 def get_credentials():
     creds_json = os.getenv("GOOGLE_CREDENTIALS")
 
+    # 🔥 ADD THIS LINE HERE
+    print("DEBUG GOOGLE_CREDENTIALS:", creds_json[:100] if creds_json else "NONE")
+
     if not creds_json:
         raise ValueError("❌ GOOGLE_CREDENTIALS not set")
 
