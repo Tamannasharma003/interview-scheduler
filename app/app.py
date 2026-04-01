4067922d25f41d1da04fde3e11a34811f48128c1
tamanna <tamanna3333333sharma@gmail.com>
2026-04-01 12:55:58 +0530

    fix flask duplication

--- app/app.py
+++ app/app.py
@@ -2,19 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-
-
-# ✅ IMPORTANT: import AFTER app creation
-import webhook 
+# ✅ import AFTER app creation
+import webhook  
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
